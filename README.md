e2237c5f0aaa3f344d384298ac074e99a59689b5
suhailvs <suhailvs@gmail.com>
2014-07-20 12:16:15 +0530

    added a simple Functional test using Selenium. It checks whether the form rendered correctly

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ django-selectize
 ================
 
 django-selectize  is a Django app based on Selectize.js that help you to create Select and Multiselect widgets in Django forms.
+
+
+Requirements
+------------
+
+* Django
+
+For testing
+-----------
+You need to install Selenium:
+
+    pip install selenium
+
+Run the tests:
+
+    ./manage.py test
--- demo/app1/tests.py
+++ demo/app1/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class MultiSelectFunctionalTests(TestCase):
+
+    base_url = 'http://localhost:8000/tests'
+    fixtures=['publications']
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+
+        #username_input = self.driver.find_element_by_name("username")
+        #username_input.send_keys(user)
+       
+    def tearDown(self):
+        self.driver.close()
+
+    def testAddArticlePage(self):
+    	"""As a visitor to the site, when I load the
+    	articles page I see the publications in 
+    	Selectize.js multiselect theme."""
+
+    	self.driver.get('{0}{1}'.format(self.base_url,'/articles/'))
+    	self.assertIn("Headline:", self.driver.find_element_by_tag_name('body').text)
