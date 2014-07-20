889a6aa707858dec4511ce7ca3e9263f2bfc2a4f
suhailvs <suhailvs@gmail.com>
2014-07-20 22:47:42 +0530

    Added to pip. Now you can use pip install django-selectize
    Updating your distribution
    Down the road, after you’ve made updates to your distribution and wish to make a new release:
    
    * increment the version number in your setup.py file,
    * update your CHANGES.txt file,
    * run `python setup.py sdist upload`
    
    see: http://bruno.im/2010/may/05/packaging-django-reusable-app/

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(name='django-selectize',
+	version='1.1',
+	description='django-selectize is a Django app based on Selectize.js that help you to create Select and Multiselect widgets in Django forms.',
+    author='Djangoer',
+    author_email='djangoer0@gmail.com',
+    url='https://github.com/djangoer/django-selectize',
+	packages=['selectize','selectize.templatetags'],#py_modules=['modoscript'],
+	include_package_data=True,#package_data = {'package' : ["selectize/static/*"] },
+	install_requires=[
+		"Django",	
+	],
+	zip_safe=False,
+)
