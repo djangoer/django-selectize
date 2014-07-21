40d51cbb7fec25e7fdedb74fa9533f064e918f5d
suhailvs <suhailvs@gmail.com>
2014-07-21 12:13:49 +0530

    Made the Template Tags DRY

--- selectize/templatetags/selectize_tags.py
+++ selectize/templatetags/selectize_tags.py
@@ -7,10 +7,25 @@ register = template.Library()
 
 @register.simple_tag
 def selectize_tags_media(media_type='css',name=''):
+	"""
+	Usage:
+	------
+	To include css media:
+	selectize_tags_media 'css' <theme>
+
+	To include Selectize Scripts:
+	selectize_tags_media 'js'
+
+	To include Selectize Scripts and Jquery:
+	selectize_tags_media 'js' 'jquery'
+	"""
 	if media_type=='js':
-		html='<script src="{url}"></script>'		
-		fpath='selectize/{name}.min.js'.format(name=name)
-	else:
-		html='<link rel="stylesheet" href="{url}">'
-		fpath='selectize/css/selectize.{name}.css'.format(name=name)
-	return html.format(url=static(fpath))
+		str_script='<script src="{url}"></script>\n'
+		html=str_script.format(url=static('selectize/selectize.min.js'))
+		if name=='jquery':
+			html=str_script.format(url=static('selectize/jquery.min.js'))+html
+		return html
+
+	if name:name+='.'
+	fpath='selectize/css/selectize.{name}css'.format(name=name)
+	return '<link rel="stylesheet" href="{url}">'.format(url=static(fpath))
